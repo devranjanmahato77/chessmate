b2ba912bdfb2b0a6d9255286ad983a219c54533b
devranjanmahato77 <devranjan.mahato.77@gmail.com>
2025-03-04 22:33:20 +0530

    Rendering Board done

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,2 +1,68 @@
 const socket = io();
 const chess = new Chess();
+
+const boardElement = document.querySelector(".chessboard");     //class ., id #
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+// Function cient side 
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row, rowindex) => {
+        row.forEach((square, squareindex) => {
+            const squareElement = document.createElement("div");
+            squareElement.classList.add("square",
+                (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+            );
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex;
+
+            if (square) {
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add(
+                    "piece", 
+                    square.color === "w" ? "white" : "black"
+                );
+                pieceElement.innerText = "";
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener("dragstart", ()=>{
+                    if(pieceElement.draggable){
+                        draggedPiece = pieceElement;
+                        sourceSquare = {row: rowindex, col: squareindex};
+                        e.dataTransfer.setData("text/plain", "");       // this will make sure during drag no platform error came
+                    }
+                });
+                pieceElement.addEventListener("dragend", (e) =>{
+                    sourceSquare = null;
+                    draggedPiece = null;
+                });
+                squareElement.appendChild(pieceElement);
+            }
+
+            squareElement.addEventListener("dragover", function(e){
+                e.preventDefault();
+            })
+            squareElement.addEventListener("drop", function(e){
+                e.preventDefault();
+                if(draggedPiece){
+                    const targetSource = {
+                        row:parseInt(squareElement.dataset.row),
+                        col:parseInt(squareElement.dataset.col)
+                    };
+                    handleMove(sourceSquare, targetSource);
+                }
+            })
+            boardElement.appendChild(squareElement);
+        });
+    });    
+};
+
+const handleMove = () => { };
+
+const getPieceUnicode = () => { };
+
+renderBoard();
